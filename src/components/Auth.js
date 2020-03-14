dd0a20f77a31ecf81b80a20f1df7d6a642f9bad9
kobylan <kobylan.kaz@gmail.com>
2020-03-14 17:29:00 +0600

    first commit

---
+++ src/components/Auth.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "../index.jpg";
+
+const Auth = () => (
+  <div className="auth">
+    <div className="logImg">
+      <img src={Image} />
+    </div>
+
+    <div className="login">
+      <input type="text" placeholder="Введите логин или email" />
+      <input type="password" placeholder="Введите пароль" />
+    </div>
+    <div className="submit">
+      <input type="submit" value="Войти" />
+    </div>
+  </div>
+);
+
+export default Auth;
